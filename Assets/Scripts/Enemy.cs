cc015d1b962226a311c52183a9ddf37cfc46a368
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2023-03-20 22:15:12 -0700

    Not-so-initial commmit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 30f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+    
+    void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+            
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+            
+        Debug.Log(waypointIndex);
+    }
+}

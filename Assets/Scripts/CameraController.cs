9f602ce9782106ed4f1b3d8bcbd002bcb29ff5c6
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2023-03-21 18:53:58 -0700

    Update after camera control

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private bool doMovement = true;
+    
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) doMovement = !doMovement;
+
+            if (!doMovement) return;
+        
+        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        
+        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        transform.position = pos;
+    }
+}
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,18 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Node : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    private void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        if (turret != null)
+        {
+            Debug.Log("Can't build there! - TODO: Display on screen.");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
     }
 }

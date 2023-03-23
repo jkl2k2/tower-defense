0bbfc46772c4ed58e78f4b2c8a9a7e6e86edded7
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2023-03-22 19:59:00 -0700

    Update after video 15

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -20,6 +20,8 @@ public class Turret : MonoBehaviour
     [Header("User Laser")]
     public bool useLaser = false;
     public LineRenderer lineRenderer;
+    public ParticleSystem impactEffect;
+    public Light impactLight;
 
     [Header("Unity Setup Fields")]
     public string enemyTag = "Enemy";
@@ -64,7 +66,12 @@ public class Turret : MonoBehaviour
         {
             if (useLaser)
             {
-                if (lineRenderer.enabled) lineRenderer.enabled = false;
+                if (lineRenderer.enabled)
+                {
+                    lineRenderer.enabled = false;
+                    impactEffect.Stop();
+                    impactLight.enabled = false;
+                }
             }
 
             return;
@@ -90,10 +97,21 @@ public class Turret : MonoBehaviour
 
     void Laser()
     {
-        if (!lineRenderer.enabled) lineRenderer.enabled = true;
+        if (!lineRenderer.enabled)
+        {
+            lineRenderer.enabled = true;
+            impactEffect.Play();
+            impactLight.enabled = true;
+        }
         
         lineRenderer.SetPosition(0, firePoint.position);
         lineRenderer.SetPosition(1, target.position);
+
+        Vector3 dir = firePoint.position - target.position;
+
+        impactEffect.transform.position = target.position + dir.normalized;
+        
+        impactEffect.transform.rotation = Quaternion.LookRotation(dir);
     }
 
     void LockOnTarget()

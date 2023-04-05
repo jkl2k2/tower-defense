55c4806cb3cda209a26aee991ddb8a5fa207d8ac
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2023-04-04 19:29:58 -0700

    Update after video 23

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,20 +9,26 @@ public class Enemy : MonoBehaviour
     [HideInInspector]
     public float speed = 10f;
     
-    public float health = 100;
-
+    public float startHealth = 100;
+    private float health;
+    
     [FormerlySerializedAs("value")] public int worth = 50;
 
     public GameObject deathEffect;
 
+    [Header("Unity Stuff")] public Image healthBar;
+
     private void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
 
     public void TakeDamage(float amount)
     {
         health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
 
         if (health <= 0)
         {
